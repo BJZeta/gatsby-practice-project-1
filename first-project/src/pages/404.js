42ece88ba26ed37173e667e27b9831f12a301bce
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-07-06 17:09:57 -0700

    designing contact page

--- first-project/src/pages/404.js
+++ first-project/src/pages/404.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/Layout"
 
 export default function NotFound() {
-    return (
-        <Layout>
-           <h1>404 NOT FOUND :(</h1> 
-        </Layout>
-    )
+  return (
+    <Layout>
+      <main className="error-page">
+        <section>
+            <h1>404</h1>
+            <h3>Page Not Found</h3>
+        </section>
+      </main>
+    </Layout>
+  )
 }
--- first-project/src/pages/contact.js
+++ first-project/src/pages/contact.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react"
+import Layout from "../components/Layout"
 
 export default function Contact() {
-    return (
-        <Layout>
-            <h1>Contact</h1>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to Get in Touch?</h3>
+            <p>
+              I'm baby poutine tofu roof party kinfolk, flexitarian microdosing
+              ugh salvia. Cray blue bottle live-edge enamel pin, ennui pabst
+              hoodie. Stumptown direct trade bitters XOXO asymmetrical,
+              shoreditch la croix shaman knausgaard skateboard.
+            </p>
+            <p>
+              Succulents hella cloud bread subway tile fingerstache pok pok ugh
+              3 wolf moon pug seitan.
+            </p>
+            <p>Blue bottle fingerstache asymmetrical XOXO.</p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">Message</label>
+                <input type="text" name="message" id="message" />
+              </div>
+              <button type="submit" className="btn block">
+                Submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
+    </Layout>
+  )
 }
